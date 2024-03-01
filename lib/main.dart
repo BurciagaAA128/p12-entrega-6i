a23293a7d7634f61deb1de362ab9580f3f7360c3
AARON_BURCIAGA_CORTES <a.21308051280321@cbtis128.edu.mx>
2024-03-01 16:01:34 +0000

    actualizar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class RutasPaginas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "App Entre Páginas",
       initialRoute: "/",
       routes: {
